24a4dc2d9b85662309cd9283b2de46e78c129e84
Ramel <dev_20@naver.com>
2023-10-26 17:11:16 +0900

    main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,15 +12,5 @@ class baseball {
 
 public class Application {
     public static void main(String[] args) {
-        System.out.print("숫자를 입력해주세요 : ");
-        String userInput = Console.readLine();
-
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
     }
 }
